3416902f22fb326aae59131fec2b8cf4d65d559e
EcomGraduates <support@ecomgraduates.com>
2023-12-28 18:16:03 -0500

    refactor(imageProcessor.js): remove redundant comments and optimize image generation process The changes were made to improve the readability of the code by removing redundant comments that were self-explanatory. The image generation process was also optimized by removing unnecessary steps and adding support for JPEG format. The image is now initially created in PNG format and then converted to JPEG if needed, which simplifies the process and improves performance.

--- imageProcessor.js
+++ imageProcessor.js
@@ -4,13 +4,12 @@ import sharp from 'sharp';
 import axios from 'axios';
 import { createCanvas, loadImage } from 'canvas';
 
-// Download an image from a URL
+
 export const downloadImage = async (url) => {
   const response = await axios({ url, responseType: 'arraybuffer' });
   return Buffer.from(response.data, 'binary');
 };
 
-// Apply opacity to the watermark
 export const applyOpacityToWatermark = async (watermarkBuffer, opacity) => {
   const image = await loadImage(watermarkBuffer);
   const canvas = createCanvas(image.width, image.height);
@@ -22,7 +21,6 @@ export const applyOpacityToWatermark = async (watermarkBuffer, opacity) => {
   return canvas.toBuffer();
 };
 
-// Add watermark to the base image
 export const addWatermark = async (baseImage, watermarkOptions) => {
   let watermarkImage;
 
@@ -57,59 +55,51 @@ export const addWatermark = async (baseImage, watermarkOptions) => {
   return finalImage;
 };
 
-// Calculate automatic font size
 export const getAutoFontSize = (width, height) => {
   return Math.round((width + height) / 2 * 0.1);
 };
 
-// Generate an image with specified options
 export const generateImage = async (options) => {
   const fontSize = options.autoFontSize ? getAutoFontSize(options.width, options.height) : options.fontSize;
 
-  const background = await sharp({
+  let background = await sharp({
     create: {
       width: options.width,
       height: options.height,
       channels: 4,
       background: options.backgroundColor
     }
-  }).toFormat(options.format).toBuffer();
+  }).toFormat('png').toBuffer();
 
   const textSVG = `<svg width="${options.width}" height="${options.height}">
     <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" 
     style="fill:${options.textColor};font-family:Arial;font-size:${fontSize}px;">${options.textOverlay}</text>
   </svg>`;
 
-  const text = await sharp({
-    create: {
-      width: options.width,
-      height: options.height,
-      channels: 4,
-      background: { r: 0, g: 0, b: 0, alpha: 0 }
-    }
-  }).composite([{
-    input: Buffer.from(textSVG),
-    gravity: 'center'
-  }]).toFormat(options.format).toBuffer();
-
   let finalImage = await sharp(background)
     .composite([{
-      input: text,
-      blend: 'over'
+      input: Buffer.from(textSVG),
+      gravity: 'center'
     }])
-    .toBuffer();
+    .toFormat('png').toBuffer();
 
   if (options.watermark) {
     finalImage = await addWatermark(finalImage, options.watermark);
   }
 
+  if (options.format === 'jpg' || options.format === 'jpeg') {
+    finalImage = await sharp(finalImage)
+      .flatten({ background: options.backgroundColor })
+      .jpeg()
+      .toBuffer();
+  }
+
   const outputDirectory = options.outputPath || '.';
   const outputFilePath = path.join(outputDirectory, `${options.filename}.${options.format}`);
   await sharp(finalImage).toFile(outputFilePath);
-  if(options.verbose) console.log(`Generated ${outputFilePath}`);
+  if (options.verbose) console.log(`Generated ${outputFilePath}`);
 };
 
-// Process a list of image generation options
 export const processList = async (listPath, options) => {
   let list;
   if (path.extname(listPath) === '.json') {
